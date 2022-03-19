5071f0ae5810fed696f465cb21a17a72bbb7d0e1
George de Pinho <georgedipinho@hotmail.com>
2022-03-19 00:36:48 -0300

    Project

---
+++ userdept/src/main/java/com/dipinho/userdept/entities/User.java
@@ -0,0 +1,48 @@
+package com.dipinho.userdept.entities;
+
+public class User {
+	private Long id;
+	private String nome;
+	private String email;
+	
+	private Department department;	//composição de objetos. assosciação entre entidades
+	
+	public User() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+		
+	
+	
+}	
