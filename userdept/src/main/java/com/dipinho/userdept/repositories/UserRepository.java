6e80e9d00eb3bd7bf1bb7a50d1ffd1a1667b5b3a
George de Pinho <georgedipinho@hotmail.com>
2022-03-20 17:16:57 -0300

    Endpoints

---
+++ userdept/src/main/java/com/dipinho/userdept/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.dipinho.userdept.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dipinho.userdept.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{	//operacoes basicas do crud
+	
+
+}
